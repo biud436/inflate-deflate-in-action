0d7919e79a81a6fed903bde4a33378fb4e4627b4
biud436 <biud436@gmail.com>
2022-01-26 17:07:45 +0900

    fixed

--- js/index.js
+++ js/index.js
@@ -1,28 +1,63 @@
-const user = {
-  id: 1,
-  name: "test",
-  currentRoom: "mainStage",
-  prevRoom: "로비",
+const entry = {
+  start() {
+    const text = document.querySelector("#normal-text").innerHTML;
+    const zip = pako.deflate(text, { to: "string", level: 1 });
+    let compressedZip = "";
+    for (let i = 0; i < zip.byteLength; i++) {
+      compressedZip += String.fromCharCode(zip[i]);
+    }
+    this.clear();
+    this.updatePrevText(text);
+    this.updateAfterText(compressedZip);
+  },
+
+  clear() {
+    const prevText = document.querySelector("#prev-text");
+    prevText.textContent = "";
+    let detail = prevText.previousElementSibling.firstElementChild;
+    detail.textContent = "";
+
+    const afterText = document.querySelector("#after-text");
+    afterText.textContent = "compressedZip";
+    detail = afterText.previousElementSibling.firstElementChild;
+    detail.textContent = "";
+  },
+
+  /**
+   * 압축되기 전의 텍스트
+   */
+  updatePrevText(user) {
+    const prevText = document.querySelector("#prev-text");
+    const userRaw = JSON.stringify(user);
+    prevText.textContent = userRaw;
+    let detail = prevText.previousElementSibling.firstElementChild;
+    detail.textContent = userRaw.length;
+    detail.style.color = "#ff0000";
+  },
+
+  /**
+   * 업축된 후의 텍스트
+   */
+  updateAfterText(compressedZip) {
+    const afterText = document.querySelector("#after-text");
+    afterText.textContent = compressedZip;
+    detail = afterText.previousElementSibling.firstElementChild;
+    detail.textContent = compressedZip.length;
+    detail.style.color = "#ff0000";
+  },
+
+  updateFrame() {
+    window.requestAnimationFrame(updateFrame);
+  },
 };
 
-const zip = pako.deflate(JSON.stringify(user), { to: "string", level: 1 });
+const connector = {
+  connect() {
+    const submit = document.querySelector("#compress-btn");
+    submit.onclick = () => {
+      entry.start();
+    };
+  },
+};
 
-let compressedZip = "";
-for (let i = 0; i < zip.byteLength; i++) {
-  compressedZip += String.fromCharCode(zip[i]);
-}
-
-// 압축되기 전의 테스트
-const prevText = document.querySelector("#prev-text");
-const userRaw = JSON.stringify(user);
-prevText.textContent = userRaw;
-let detail = prevText.previousElementSibling.firstElementChild;
-detail.textContent = userRaw.length;
-detail.style.color = "#ff0000";
-
-// 압축된 텍스트
-const afterText = document.querySelector("#after-text");
-afterText.textContent = compressedZip;
-detail = afterText.previousElementSibling.firstElementChild;
-detail.textContent = compressedZip.length;
-detail.style.color = "#ff0000";
+connector.connect();
